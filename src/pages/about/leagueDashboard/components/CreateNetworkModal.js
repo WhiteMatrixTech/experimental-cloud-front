7c78eb617a3b1872e3bb2965555a6d1646adecac
liyw <1539603898@qq.com>
2021-04-20 14:30:45 +0800

    add nodes when create network

--- src/pages/about/leagueDashboard/components/CreateNetworkModal.js
+++ src/pages/about/leagueDashboard/components/CreateNetworkModal.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
-import { Button, Form, Input, Modal, Select } from 'antd';
+import { Button, Form, Input, Modal, Select, Space, Radio, Row, Col } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import { connect } from 'dva';
+import styles from './CreateNetworkModal.less';
 
 const { Item } = Form;
 const { Option } = Select;
@@ -14,6 +16,12 @@ const formItemLayout = {
   },
 };
 
+const inlineItemLayout = {
+  wrapperCol: {
+    sm: { span: 24 },
+  },
+};
+
 function CreateNetworkModal(props) {
   const { dispatch, visible, onCancel, createLoading = false, User, ElasticServer } = props;
   const { serverList } = ElasticServer;
@@ -24,11 +32,14 @@ function CreateNetworkModal(props) {
     form
       .validateFields()
       .then(async (values) => {
-        const { serverName, ...rest } = values;
-        let params = { ...rest };
-        if (values.serverName) {
-          params.serverName = values.serverName;
-        }
+        const { networkTemplate, initPeerInfo, ...rest } = values;
+        const peerList = initPeerInfo.map((peer) => {
+          if (!peer.serverName) {
+            delete peer.serverName;
+          }
+          return peer;
+        });
+        const params = { ...rest, initPeerInfo: peerList };
         const res = await dispatch({
           type: 'Dashboard/createNetwork',
           payload: params,
@@ -73,6 +84,16 @@ function CreateNetworkModal(props) {
   return (
     <Modal {...drawerProps}>
       <Form {...formItemLayout} form={form}>
+        <Item label="网络模板" initialValue="default" name="networkTemplate">
+          <Radio.Group className={styles['radio-group']}>
+            <Radio className={styles.radio} value="default">
+              默认模板
+            </Radio>
+            <Radio disabled className={styles.radio} value="custom">
+              自定义模板
+            </Radio>
+          </Radio.Group>
+        </Item>
         <Item
           label="网络名称"
           name="networkName"
@@ -126,48 +147,104 @@ function CreateNetworkModal(props) {
         >
           <Input placeholder="请输入组织别名" />
         </Item>
-        <Item
-          label="节点名称"
-          name="initPeerName"
-          initialValue=""
-          rules={[
-            {
-              required: true,
-              message: '请输入节点名称',
-            },
-            {
-              min: 3,
-              max: 15,
-              type: 'string',
-              pattern: /^[a-zA-Z0-9]+$/,
-              message: '节点名称由3~15位英文或数字组成',
-            },
-          ]}
-        >
-          <Input placeholder="请输入节点名称" />
-        </Item>
-        <Item
-          label="节点别名"
-          name="initPeerAliasName"
-          initialValue=""
-          rules={[
-            {
-              required: true,
-              message: '请输入节点别名',
-            },
-          ]}
-        >
-          <Input placeholder="请输入节点别名" />
-        </Item>
-        <Item label="服务器" name="serverName" tooltip="不选择则使用默认服务器">
-          <Select allowClear getPopupContainer={(triggerNode) => triggerNode.parentNode} placeholder="选择服务器">
-            {serverList.map((item) => (
-              <Option key={item.serverName} value={item.serverName}>
-                {item.serverName}
-              </Option>
-            ))}
-          </Select>
-        </Item>
+        <Form.List name="initPeerInfo">
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name, fieldKey, ...restField }) => (
+                <div key={key}>
+                  <Row gutter="24">
+                    <Col span={11}>
+                      <Item
+                        {...restField}
+                        {...inlineItemLayout}
+                        name={[name, 'nodeName']}
+                        fieldKey={[fieldKey, 'nodeName']}
+                        rules={[
+                          {
+                            required: true,
+                            message: '请输入节点名称',
+                          },
+                          {
+                            min: 3,
+                            max: 15,
+                            type: 'string',
+                            pattern: /^[a-zA-Z0-9]+$/,
+                            message: '节点名称由3~15位英文或数字组成',
+                          },
+                        ]}
+                      >
+                        <Input placeholder="请输入节点名称" />
+                      </Item>
+                    </Col>
+                    <Col span={11}>
+                      <Item
+                        {...restField}
+                        {...inlineItemLayout}
+                        name={[name, 'nodeAliasName']}
+                        fieldKey={[fieldKey, 'nodeAliasName']}
+                        rules={[{ required: true, message: '请输入节点别名' }]}
+                      >
+                        <Input placeholder="请输入节点别名" />
+                      </Item>
+                    </Col>
+                    <Col span={11}>
+                      <Item
+                        {...restField}
+                        {...inlineItemLayout}
+                        name={[name, 'serverName']}
+                        fieldKey={[fieldKey, 'serverName']}
+                      >
+                        <Select
+                          allowClear
+                          placeholder="选择服务器"
+                          style={{ width: '100%' }}
+                          getPopupContainer={(triggerNode) => triggerNode.parentNode}
+                        >
+                          {serverList.map((item) => (
+                            <Option key={item.serverName} value={item.serverName}>
+                              {item.serverName}
+                            </Option>
+                          ))}
+                        </Select>
+                      </Item>
+                    </Col>
+                    <Col span={11}>
+                      <Item
+                        {...restField}
+                        {...inlineItemLayout}
+                        initialValue={true}
+                        name={[name, 'isAnchor']}
+                        fieldKey={[fieldKey, 'isAnchor']}
+                      >
+                        <Select
+                          allowClear
+                          placeholder="是否为anchor节点"
+                          style={{ width: '100%' }}
+                          getPopupContainer={(triggerNode) => triggerNode.parentNode}
+                        >
+                          <Option key="true" value={true}>
+                            anchor节点
+                          </Option>
+                          <Option key="false" value={false}>
+                            非anchor节点
+                          </Option>
+                        </Select>
+                      </Item>
+                    </Col>
+                    <Col span={1}>
+                      <MinusCircleOutlined onClick={() => remove(name)} />
+                    </Col>
+                  </Row>
+                </div>
+              ))}
+              <Item {...inlineItemLayout}>
+                <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                  添加节点
+                </Button>
+              </Item>
+            </>
+          )}
+        </Form.List>
       </Form>
     </Modal>
   );
