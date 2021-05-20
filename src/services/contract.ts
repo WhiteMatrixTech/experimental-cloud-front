a9f7485a368f2d670a6d3af6f0b6ec9f23165878
liyw <1539603898@qq.com>
2021-05-20 16:57:26 +0800

    add contact & rbac api params type

--- src/services/contract.ts
+++ src/services/contract.ts
@@ -1,5 +1,7 @@
 import { request } from '../utils/request';
 import { ChainCodeSchema } from 'umi';
+import { AllPaginationParams, BasicApiParams } from '@/utils/types';
+import { ChainCodeStatus } from '@/pages/about/contract/_config';
 
 /**
  * 创建合约
@@ -11,76 +13,90 @@ export async function addContract(params: ChainCodeSchema) {
 /**
  * 审核合约
  */
-export async function verifyContract(params) {
+export async function verifyContract(
+  params: BasicApiParams & {
+    chainCodeName: string;
+    VerifyStatus: ChainCodeStatus;
+  },
+) {
   return request(`/network/${params.networkName}/chainCodes/verifyChainCode`, { method: 'POST', body: params });
 }
 
 /**
  * 安装合约
  */
-export async function installContract(params) {
+export async function installContract(params: BasicApiParams) {
   return request(`/network/${params.networkName}/chainCodes/install`, { method: 'POST', body: params });
 }
 
 /**
  * 升级合约
  */
-export async function upgradeContract(params) {
+export async function upgradeContract(params: ChainCodeSchema) {
   return request(`/network/${params.networkName}/chainCodes/upgrade`, { method: 'POST', body: params });
 }
 
 /**
  * 发布合约
  */
-export async function releaseContract(params) {
+export async function releaseContract(params: any) {
   return request(`/network/${params.networkName}/chainCodes/approve`, { method: 'POST', body: params });
 }
 
+export interface InvokeChainCodeRequest {
+  networkName: string;
+  channelId: string;
+  chainCodeName: string;
+  methodName: string;
+  params: string[];
+  userId: string;
+  isInit: boolean;
+}
 /**
  * 调用合约--invoke
  */
-export async function invokeChainCodeMethod(params) {
+export async function invokeChainCodeMethod(params: InvokeChainCodeRequest) {
   return request(`/network/${params.networkName}/chainCodes/invokeChainCodeMethod`, { method: 'POST', body: params });
 }
 
 /**
  * 调用合约--query
  */
-export async function queryChainCodeMethod(params) {
+export async function queryChainCodeMethod(params: InvokeChainCodeRequest) {
   return request(`/network/${params.networkName}/chainCodes/queryChainCodeMethod`, { method: 'POST', body: params });
 }
 
 /**
  * 使用中的通道列表
  */
-export async function getChannelList(params) {
+export async function getChannelList(params: BasicApiParams) {
   return request(`/network/${params.networkName}/channels/listChannelsInUse`);
 }
 
 /**
  * 当前用户所在通道列表
  */
-export async function getChannelListByOrg(params) {
+export async function getChannelListByOrg(params: BasicApiParams) {
   return request(`/network/${params.networkName}/channels/listChannelsOfOrg`);
 }
 
 /**
  * 检查是否有组织 且组织在使用中
  */
-export async function checkOrgInUse(params) {
+export async function checkOrgInUse(params: BasicApiParams) {
   return request(`/network/${params.networkName}/orgs/checkOrgInUse`, { method: 'POST' });
 }
 
 /**
  * 我的合约 - 合约列表
  */
-export async function getChainCodeList(params) {
+export async function getChainCodeList(params: BasicApiParams & AllPaginationParams) {
   return request(`/network/${params.networkName}/chainCodes/query`, { method: 'POST', body: params });
 }
 
 /**
  * 我的合约 - 合约总数
  */
-export async function getChainCodeTotal(params) {
+export async function getChainCodeTotal(params: BasicApiParams) {
   return request(`/network/${params.networkName}/chainCodes/totalCount`);
 }
--- src/services/rbac.ts
+++ src/services/rbac.ts
@@ -1,50 +1,52 @@
+import { RbacRole } from '@/models/rbac';
+import { BasicApiParams, BasicPaginationParams } from '@/utils/types';
 import { request } from '../utils/request';
 
 /**
  * 获取角色列表
  */
-export async function getRoleList(params) {
+export async function getRoleList(params: BasicApiParams & BasicPaginationParams) {
   return request(`/network/${params.networkName}/accessPolicy/listRoles`, { method: 'POST', body: params });
 }
 
 /**
  * 获取角色名列表
  */
-export async function getRoleNameList(params) {
+export async function getRoleNameList(params: BasicApiParams) {
   return request(`/network/${params.networkName}/accessPolicy/listRoleNames`, { method: 'POST', body: params });
 }
 
 /**
  * 获取合约列表
  */
-export async function getChaincodeList(params) {
+export async function getChaincodeList(params: any) {
   return request(`/network/${params.networkName}/chainCodes/queryAll/${params.companyName}`);
 }
 
 /**
  * 获取自己的合约列表
  */
-export async function getMyselfChainCodeList(params) {
+export async function getMyselfChainCodeList(params: any) {
   return request(`/network/${params.networkName}/chainCodes/queryOwn/${params.companyName}`);
 }
 
 /**
  * 获取角色的访问策略配置
  */
-export async function getRbacConfigWithRole(params) {
+export async function getRbacConfigWithRole(params: BasicApiParams & { roleName: string }) {
   return request(`/network/${params.networkName}/accessPolicy/${params.roleName}`);
 }
 
 /**
  * 添加配置 角色
  */
-export async function setConfig(params) {
+export async function setConfig(params: RbacRole & { networkName: string }) {
   return request(`/network/${params.networkName}/accessPolicy/create`, { method: 'POST', body: params });
 }
 
 /**
  * 输入JSON配置访问策略
  */
-export async function setConfigByJson(params) {
+export async function setConfigByJson(params: RbacRole & { networkName: string }) {
   return request(`/network/${params.networkName}/accessPolicy/createByJson`, { method: 'POST', body: params });
 }
