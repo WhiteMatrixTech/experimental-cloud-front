8b5c0e2b9c05044dcaf0f5b4311208cfc41d3b5b
临安 <2591088659@qq.com>
2021-05-26 19:51:30 +0800

    feature/storage (#46)

--- src/services/custom-image.ts
+++ src/services/custom-image.ts
@@ -1,9 +1,23 @@
+import { ImageDetail } from '~/models/custom-image';
 import { AllPaginationParams } from '~/utils/types';
 import { request } from '../utils/request';
 
-/**
- * 获取镜像列表
- */
+//获取镜像列表
 export async function getImageList(params: AllPaginationParams) {
   return request('/image/query', { method: 'POST', body: params });
+}
+
+//获取镜像列表总数
+export async function getImageListTotal() {
+  return request('/image/totalCount', { method: 'POST', body: {} });
+}
+
+//添加自定义镜像
+export async function addCustomImage(params: ImageDetail) {
+  return request('/image/create', { method: 'POST', body: params });
+}
+
+//删除镜像
+export async function deleteCustomImage(params: { imageId: number }) {
+  return request(`/image/${params.imageId}`, { method: 'DELETE' });
 }
