3a268613869b11d5249ba737a108f89aa4d41bd8
liyw <1539603898@qq.com>
2021-06-23 13:56:11 +0800

    feat::art:modify paltform logo & name

--- src/components/Layout/LoginLayout/LoginFooter/index.tsx
+++ src/components/Layout/LoginLayout/LoginFooter/index.tsx
@@ -9,7 +9,7 @@ export default class LoginFooter extends React.PureComponent {
   render() {
     return (
       <Footer className={styles.footer}>
-        <div className={styles.copyright}>Powered By BUAA | 违法和不良信息举报电话：0571-85366069</div>
+        <div className={styles.copyright}>学校地址：江西省南昌市先锋创客小镇  |  邮编：330041  |  电话：0791-83792966  |  传真：0791-87709377  |  ©2020 江西软件职业技术大学版权所有</div>
       </Footer>
     );
   }
