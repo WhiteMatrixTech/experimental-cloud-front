cc604f5fd4991c97ca9e095618b9a7e3ae471ec9
liyw <1539603898@qq.com>
2021-06-23 13:56:11 +0800

    feat::art:modify paltform logo & name

--- src/components/TopHeader/index.tsx
+++ src/components/TopHeader/index.tsx
@@ -6,7 +6,7 @@ import type { Dispatch } from 'umi';
 import { Layout, Menu, Dropdown } from 'antd';
 import { DownOutlined, BarsOutlined } from '@ant-design/icons';
 import { ConnectState } from '~/models/connect';
-import buaaLogo from '~/assets/images/buaa-small.png';
+import buaaLogo from '~/assets/images/jiangxizhiye-small.png';
 import styles from './index.less';
 
 const { Header } = Layout;
