c8c57639dd95bb952188be4583f57c3b9e64bd6d
hexi1997 <18715156450@163.com>
2023-06-30 13:56:54 +0800

    feat: update copywriter

--- src/components/Layout/LoginLayout/LoginFooter/index.tsx
+++ src/components/Layout/LoginLayout/LoginFooter/index.tsx
@@ -9,7 +9,7 @@ export default class LoginFooter extends React.PureComponent {
   render() {
     return (
       <Footer className={styles.footer}>
-        <div className={styles.copyright}>Powered By BUAA | 违法和不良信息举报电话：0571-85366069</div>
+        <div className={styles.copyright}>Powered By SIAS</div>
       </Footer>
     );
   }
